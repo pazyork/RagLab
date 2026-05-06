3c33c9528216ed22414cf7b674031d7b357a7394
fanpengcheng <pazyorkcc@gmail.com>
2026-05-07 02:14:04 +0800

    chore: remove legacy NiceGUI UI code
    The UI has been fully migrated to Vue 3 + FastAPI.
    These NiceGUI files are no longer imported anywhere.

--- src/raglab/ui/components/__init__.py
+++
@@ -1,3 +0,0 @@
-from .heatmap import render_heatmap
-
-__all__ = ["render_heatmap"]
--- src/raglab/ui/pages/__init__.py
+++
@@ -1,5 +0,0 @@
-from .playground import render_playground
-from .datasets import render_datasets
-from .settings import render_settings
-
-__all__ = ["render_playground", "render_datasets", "render_settings"]
